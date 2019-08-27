bc028fb3e59214d505c2251c122e73d7f1fc705b
Ben Ogie <bensoft2k5@gmail.com>
2019-08-27 16:12:51 +0100

    added routes

---
+++ routes/auth/index.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import STORE from '../../data/dummy-user';
+import {
+  AUTH_TYPES
+} from '../../util/constants';
+import {
+  createHashSignature
+} from '../../util/helpers';
+import axios from 'axios'
+
+const router = express.Router();
+const PSS_BASE_URL = process.env.PSS_BASE_URL;
+const tenantId = process.env.TENANT_ID;
+const secret = process.env.SECRET;
+
+router.post('/', (req, res) => {
+  const userExternalId = STORE.users[0].uuid;
+  const authParams = {
+    guiText: 'Do you okay this transaction',
+    guiHeader: 'Authorization requested'
+  };
+  const type = AUTH_TYPES.OK
+  const hashStr = `${tenantId}${userExternalId}${secret}`;
+  const signature = createHashSignature(hashStr);
+  console.log(signature);
+
+  axios({
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${PSS_BASE_URL}/gateway/auth`,
+      data: {
+        tenantId,
+        userExternalId,
+        type,
+        authParams,
+        signature
+      }
+    })
+    .then((response) => {
+      res.json({
+        msg: 'Authentication successful',
+        data: response.data
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        msg: 'Authentication unsuccessful',
+        data: error.response.data
+      });
+    });
+
+})
+
+export default router
--- routes/linking/index.js
+++ routes/linking/index.js
@@ -1,10 +1,44 @@
-import express from 'express'
-
+import express from 'express';
+import fetch from 'cross-fetch';
+import STORE from '../../data/dummy-user';
+import { AUTH_TYPES } from '../../util/constants';
+import { createHashSignature } from '../../util/helpers';
+import axios from 'axios'
 
 const router = express.Router();
+const PSS_BASE_URL = process.env.PSS_BASE_URL;
+const tenantId = process.env.TENANT_ID;
+const secret = process.env.SECRET;
 
-router.get('/link', (req, res) => {
-
+router.post('/', (req, res) => {
+  const userExternalId = STORE.users[0].uuid;
+	const hashStr = `${tenantId}${userExternalId}${secret}`;
+  const signature = createHashSignature(hashStr);
+  
+	axios({
+    method: 'post',
+		headers: {
+			'Content-Type': 'application/json'
+    },
+    url: `${PSS_BASE_URL}/gateway/link`,
+		data: {
+      tenantId,
+      userExternalId,
+      signature
+    }
+	})
+		.then((response) => {
+			console.log(response.data);
+			res.json({
+        msg: 'Linking successful',
+        data: response.data
+			});
+		})
+		.catch((error) => {
+			res.status(500).json({
+				msg: 'Linking unsuccessful'
+      });
+		});
 });
 
 export default router;
